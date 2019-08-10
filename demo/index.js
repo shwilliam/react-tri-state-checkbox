ac34e0698ea983cc15a22d7ce1c8af1548701dd9
William L <w-lindvall@outlook.com>
2019-08-09 21:31:11 -0700

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TriStateCheckbox from '../dist'
+import {Checkbox, TriStateCheckbox, TriStateContext} from '../dist'
+import '../dist/styles.css'
 
-function Demo() {
-  return (
-    <div>
-      <TriStateCheckbox />
-    </div>
-  )
-}
+const Demo = () => (
+  <TriStateContext controls={['cond1', 'cond2', 'cond3', 'cond4']}>
+    <fieldset>
+      <legend>Sandwich Condiments</legend>
+      <label style={{display: 'block', lineHeight: '2.6rem'}}>
+        <TriStateCheckbox>All condiments</TriStateCheckbox>
+      </label>
+      <label style={{display: 'block', lineHeight: '2.6rem'}}>
+        <Checkbox id="cond1" checked />
+        Lettuce
+      </label>
+      <label style={{display: 'block', lineHeight: '2.6rem'}}>
+        <Checkbox id="cond2" />
+        Tomato
+      </label>
+      <label style={{display: 'block', lineHeight: '2.6rem'}}>
+        <Checkbox id="cond3" />
+        Mustard
+      </label>
+      <label style={{display: 'block', lineHeight: '2.6rem'}}>
+        <Checkbox id="cond4" />
+        Sprouts
+      </label>
+    </fieldset>
+  </TriStateContext>
+)
 
 const el = document.getElementById('demo')
 ReactDOM.render(<Demo />, el)
