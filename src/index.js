82a2158d507ea665f4dfa8e9702d67a76aa21383
William L <w-lindvall@outlook.com>
2019-08-09 20:23:09 -0700

    set up babel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+TriStateCheckbox.propTypes = {}
+
+TriStateCheckbox.defaultProps = {}
+
+function TriStateCheckbox({...props}) {
+  return <div>Hello world</div>
+}
+
+export default TriStateCheckbox
